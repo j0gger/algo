731fcdc7864054f157f6c6a31f1c5febda38d4a1
Sachin Manpathak <smanpathak@Sachins-MacBook-Pro.local>
2018-02-23 19:02:05 -0800

    Add K-th smallest

--- srch/search.go
+++ srch/search.go
@@ -7,7 +7,7 @@ import (
 
 func Partition(data []int, l int, r int) (int, error) {
 	if l > r {
-		return 0, fmt.Errorf("Lower bound:", l, " cannot be larger than upper bound:", r)
+		return 0, fmt.Errorf("Lower bound: %d cannot be larger than upper bound: %d", l, r)
 	}
 
 	pivot := data[r]
@@ -32,4 +32,27 @@ func Partition(data []int, l int, r int) (int, error) {
 	data[r] = data[i]
 	data[i] = pivot
 	return i, nil
+}
+
+
+func FindSmallest(data []int, l int, r int, k int) error {
+	fmt.Println(l, r, k)
+	if k > len(data) {
+		return fmt.Errorf("k cannot be larger than length of data")
+	}
+
+	pivot, err := Partition(data, l, r)
+	if err != nil {
+		return err
+	}
+
+	if pivot > l + k - 1 {
+		return FindSmallest(data, l, pivot - 1, k)
+	}
+
+	if pivot == l + k - 1 {
+		return nil
+	}
+
+	return FindSmallest(data, pivot + 1, r, k - pivot - 1)
 }
--- srch/search_test.go
+++ srch/search_test.go
@@ -8,6 +8,26 @@ import (
 func TestPartition(t *testing.T) {
 	vals := []int{5, 11, 6, 99, 81, 41, 64}
 	idx, err := Partition(vals, 0, len(vals) - 1)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, idx, 4)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 4, idx)
 }
+
+
+func TestFindSmallest(t *testing.T) {
+	vals := []int{55, 11, 10, 34, 14, 12, 26}
+	err := FindSmallest(vals, 0, len(vals) - 1, 6)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, []int{12, 11, 10, 14, 26, 34}, vals[:6])
+}
+
+
+
+func TestFindSmallest2(t *testing.T) {
+	vals := []int{3, 2, 1}
+	err := FindSmallest(vals, 0, len(vals) - 1, 2)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, []int{1, 2}, vals[:2])
+}
+
